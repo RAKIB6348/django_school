c80c5a10b5cf8103381459f229721b9bfd885ac0
RAKIB6348 <shahidulhuda0@gmail.com>
2026-08-22 17:34:26 +0600

    add CustomUser model with admin/teacher/student user types

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    USER_TYPE_CHOICES = (
+        ('admin', 'Admin'),
+        ('teacher', 'Teacher'),
+        ('student', 'Student'),
+    )
+    user_type = models.CharField(max_length=10, choices=USER_TYPE_CHOICES)
