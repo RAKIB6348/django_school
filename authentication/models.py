f29506a824cab5ab3a32d24b9ecb54feb99fce54
RAKIB6348 <shahidulhuda0@gmail.com>
2026-08-22 17:36:23 +0600

    add profile pic in customuser models

--- authentication/models.py
+++ authentication/models.py
@@ -9,3 +9,4 @@ class CustomUser(AbstractUser):
         ('student', 'Student'),
     )
     user_type = models.CharField(max_length=10, choices=USER_TYPE_CHOICES)
+    profile_pic = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
