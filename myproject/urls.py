755ac81c4a125396bb094a25a597a41272dfde91
RAKIB6348 <shahidulhuda0@gmail.com>
2026-08-22 13:43:45 +0600

    update urls

--- myproject/urls.py
+++ myproject/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('login/', views.login, name='login'),
+    path('bashboard/', views.index, name='index'),
+    path('', views.login, name='login'),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
