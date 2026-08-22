37c10a4a8b3eeac22a7609f9fca6f56b07476e47
RAKIB6348 <shahidulhuda0@gmail.com>
2026-08-22 18:34:57 +0600

    add profile page with extends base, load static, and user data

--- myproject/urls.py
+++ myproject/urls.py
@@ -22,6 +22,7 @@ from . import views
 
 urlpatterns = [
     path('dashboard/', views.index, name='index'),
+    path('profile/', views.profile, name='profile'),
     path('', views.login, name='login'),
     path('admin/', admin.site.urls),
     path('auth/', include('authentication.urls')),
--- myproject/views.py
+++ myproject/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 
+@login_required
 def index(request):
-    return render(request, 'Admin/dashboard.html')
+    if request.user.user_type == 'admin':
+        return render(request, 'Admin/dashboard.html')
+    return render(request, 'base.html')
 
 
 def login(request):
     return render(request, 'login.html')
+
+
+@login_required
+def profile(request):
+    return render(request, 'profile.html')
