a178f2e421888b73d1fe3e5fb38e1a7ddb2b8e12
RAKIB6348 <shahidulhuda0@gmail.com>
2026-08-22 17:55:20 +0600

    user login

--- myproject/settings.py
+++ myproject/settings.py
@@ -127,11 +127,7 @@ MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 
-# Email
-# https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
-
-MAILERS = {
-    'default': {
-        'BACKEND': 'django.core.mail.backends.console.EmailBackend',
-    },
-}
+# Login
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'index'
+LOGOUT_REDIRECT_URL = 'login'
--- myproject/urls.py
+++ myproject/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
@@ -24,4 +24,5 @@ urlpatterns = [
     path('dashboard/', views.index, name='index'),
     path('', views.login, name='login'),
     path('admin/', admin.site.urls),
+    path('auth/', include('authentication.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
