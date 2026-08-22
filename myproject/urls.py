2d425974e5e40ba1a991b40d2c392d166aed07b3
RAKIB6348 <shahidulhuda0@gmail.com>
2026-08-22 13:41:37 +0600

    add login files,urls,views

--- myproject/urls.py
+++ myproject/urls.py
@@ -22,5 +22,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login/', views.login, name='login'),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
